350137531cc04e7344b6b0a71c26e57d2aacbf4b
Benedikt Brandner <benedikt.brandner@gmail.com>
2016-06-11 18:44:18 +0200

    stub for restructuring

---
+++ evaluation/evaluator.py
@@ -0,0 +1,12 @@
+
+
+class Evaluator(object):
+    def __init__(self, strategy):
+        """
+        :param strategy: How to evaluate:
+            one-off: always feed ground truth
+        """
+        pass
+
+    def evaluate(self, model, data_x):
+        pass
