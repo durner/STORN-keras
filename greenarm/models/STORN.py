f15c1051749c589d08a8025b751750f01606ff16
Dominik Durner <durner@in.tum.de>
2016-06-16 15:43:05 +0200

    * Reworked the STORN model build method

--- greenarm/models/STORN.py
+++ greenarm/models/STORN.py
@@ -13,21 +13,32 @@ from greenarm.models.sampling.sampling import sample_gauss
 
 class STORNRecognitionModel:
     def __init__(self):
-        self.sample_z = 0
-        self.rnn_recogn_stats = 0
-        self.input_layer = 0
+        self.train_rnn_recogn_stats = 0
+        self.train_input = 0
+        self.train_z = 0
+        self.predict_rnn_recogn_stats = 0
+        self.preduct_input = 0
+        self.predict_z = 0
 
-    def build(self, seq_shape, joint_shape):
-        self.input_layer = Input(shape=(seq_shape, joint_shape))
-        # input_layer = Input(batch_shape=(1, 1, joint_shape))
+    def _build(self, phase, seq_shape, joint_shape):
+        if phase == "train":
+            input_layer = Input(shape=(seq_shape, joint_shape))
+        else:
+            input_layer = Input(batch_shape=(1, 1, joint_shape))
 
-        embed1 = TimeDistributed(Dense(32, activation="tanh"))(self.input_layer)
+        embed1 = TimeDistributed(Dense(32, activation="tanh"))(input_layer)
         embed1 = Dropout(0.3)(embed1)
         rnn_recogn = GRU(128, return_sequences=True, dropout_W=0.2, dropout_U=0.2)(embed1)
-        self.rnn_recogn_stats = TimeDistributed(Dense(14, activation="relu"))(rnn_recogn)
+        rnn_recogn_stats = TimeDistributed(Dense(14, activation="relu"))(rnn_recogn)
 
         # sample z from the distribution in X
-        self.sample_z = TimeDistributed(Lambda(self.do_sample, output_shape=(joint_shape,)))(self.rnn_recogn_stats)
+        sample_z = TimeDistributed(Lambda(self.do_sample, output_shape=(joint_shape,)))(rnn_recogn_stats)
+
+        return rnn_recogn_stats, input_layer, sample_z
+
+    def build(self, seq_shape, joint_shape):
+        self.train_rnn_recogn_stats, self. train_input, self.train_z = self._build("train", seq_shape, joint_shape)
+        self.predict_rnn_recogn_stats, self.preduct_input, self.predict_z = self._build("predict", seq_shape, joint_shape)
 
     def predict(self):
         pass
@@ -45,19 +56,38 @@ class STORNRecognitionModel:
 
 class STORNGeneratingModel:
     def __init__(self):
-        self.rnn_gen_stats = 0
+        self.train_model = 0
+        self.train_input = 0
+        self.predict_model = 0
+        self.preduct_input = 0
 
-    def build(self, recognition_model, seq_shape, joint_shape):
-        input_layer = Input(shape=(seq_shape, joint_shape))
-        gen_input = merge(inputs=[input_layer, recognition_model.sample_z], mode='concat')
+    def _build(self, phase, recognition_model, seq_shape, joint_shape):
+        if phase == "train":
+            input_layer = Input(shape=(seq_shape, joint_shape))
+            rec_z = recognition_model.train_z
+            rec_input = recognition_model.train_input
+            rec = recognition_model.train_rnn_recogn_stats
+        else:
+            input_layer = Input(batch_shape=(1, 1, joint_shape))
+            rec_z = recognition_model.predict_z
+            rec_input = recognition_model.predict_input
+            rec = recognition_model.predict_rnn_recogn_stats
+
+        gen_input = merge(inputs=[input_layer, rec_z], mode='concat')
         embed2 = TimeDistributed(Dense(32, activation="relu"), input_shape=(seq_shape, 2*joint_shape))(gen_input)
         embed2 = Dropout(0.3)(embed2)
         rnn_gen = GRU(128, return_sequences=True, dropout_W=0.2, dropout_U=0.2)(embed2)
-        self.rnn_gen_stats = TimeDistributed(Dense(14, activation="relu"))(rnn_gen)
+        rnn_gen_stats = TimeDistributed(Dense(14, activation="relu"))(rnn_gen)
 
-        output = merge([self.rnn_gen_stats, recognition_model.rnn_recogn_stats], mode='concat')
-        model = Model(input=[recognition_model.input_layer, input_layer], output=output)
+        output = merge([rnn_gen_stats, rec], mode='concat')
+        model = Model(input=[rec_input, input_layer], output=output)
         model.compile(optimizer='rmsprop', loss=keras_variational)
+
+        return model, input_layer
+
+    def build(self, recognition_model, seq_shape, joint_shape):
+        self.train_model, self.train_input = self._build("train", recognition_model, seq_shape, joint_shape)
+        self.predict_model, self.preduct_input = self._build("predict", recognition_model, seq_shape, joint_shape)
 
     def predict(self):
         pass
