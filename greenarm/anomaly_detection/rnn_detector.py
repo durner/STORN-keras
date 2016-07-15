0e71942449232a2b4ef6fb33d8d6aedd1ca7255d
Philipp Dowling <phdowling@users.noreply.github.com>
2016-07-15 14:40:54 +0200

    saving

---
+++ greenarm/anomaly_detection/rnn_detector.py
@@ -0,0 +1,16 @@
+
+# Basic idea: run the data through the STORN model, and get back
+#   - error (KL especially)
+#   - prediction
+# Combine this with the original data, and feed an RNN a concat-merge of these
+
+
+class RNNAnomalyDetector(object):
+    def __init__(self, storn_model):
+        pass
+
+    def build_model(self):
+        pass
+
+    def train(self, X, y):
+        pass
