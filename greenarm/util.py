3c2b535279a7a5daa5e22e5148321fe9f6b5ecd2
Philipp Dowling <phdowling@users.noreply.github.com>
2016-07-18 15:04:01 +0200

    saving

--- greenarm/util.py
+++ greenarm/util.py
@@ -10,6 +10,9 @@ def get_logger(name):
     return logging.getLogger(name)
 
 
+logger = get_logger(__name__)
+
+
 def generate_shifted(data, predict_forward=1):
     return data[:, :-predict_forward, :], data[:, predict_forward:, :]
 
@@ -35,3 +38,36 @@ def subsample(sequence, step):
             result.append(sequence[i, :])
             prev = current_timestamp
     return np.asarray(result, dtype='float32')
+
+
+def shuffle_together(*arrays):
+    new_indexes = range(arrays[0].shape[0])
+    random.shuffle(new_indexes)
+    return [array[new_indexes] for array in arrays]
+
+
+def print_eval(predicted, ground_truth):
+    tp, fp, tn, fn = 0.000000001, 0.000000001, 0.000000001, 0.000000001
+    total = predicted.shape[0]
+    corrects = 0.
+    for p, gt in zip(predicted, ground_truth):
+        if p == gt:
+            corrects += 1.
+            if gt:
+                tp += 1.
+            else:
+                tn += 1.
+        else:
+            if gt:
+                fn += 1.
+            else:
+                fp += 1.
+    P = (tp / tp + fp)
+    R = (tp / tp + fn)
+    logger.debug("Total: %s. Positives: %s. Negatives: %s" % (total, predicted.sum(), total - predicted.sum()))
+    logger.debug("TP: %s. FP: %s. TN: %s. FN: %s." % (tp, fp, tn, fn))
+    logger.debug("Accuracy: %s" % (corrects / float(total)))
+    logger.debug("Precision: %s" % P)
+    logger.debug("Recall (Sensitivity): %s" % R)
+    logger.debug("TN Rate (Specificity): %s" % (tn / tn + fp))
+    logger.debug("F1: %s" % ((2. * P * R) / (P + R)))
