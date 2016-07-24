6cea3e8b28704c22024bae3e46b1c91f712b4550
Dominik Durner <durner@in.tum.de>
2016-07-24 17:38:39 +0200

    * Tweaked Neural Network Detector with Dropout
    94% acc, 89% precession, 83% sensitivity

--- greenarm/anomaly_detection/nn_detector.py
+++ greenarm/anomaly_detection/nn_detector.py
@@ -1,6 +1,6 @@
 from keras.callbacks import ModelCheckpoint, EarlyStopping
 from keras.models import Sequential
-from keras.layers import Dense, Activation
+from keras.layers import Dense, Activation, Dropout
 from greenarm.util import get_logger
 import time
 import numpy
@@ -22,20 +22,23 @@ class NNAnomalyDetector(object):
     @staticmethod
     def build_model(seq_len=None):
         model = Sequential()
-        model.add(Dense(200, input_shape=(seq_len,)))
+        model.add(Dense(1536, input_shape=(seq_len,)))
         model.add(Activation("relu"))
-        model.add(Dense(5))
+        model.add(Dropout(0.5))
+        model.add(Dense(128))
         model.add(Activation("relu"))
+        model.add(Dropout(0.32))
         model.add(Dense(output_dim=1))
         model.add(Activation("sigmoid"))
         model.compile(optimizer='sgd', loss='binary_crossentropy', metrics=['acc'])
         return model
 
-    def train(self, X, y, validation_split=0.1, max_epochs=2000):
+    def train(self, X, y, validation_split=0.1, max_epochs=500):
         n_samples = X.shape[0]
         seq_len = X.shape[1]
         X = numpy.reshape(X, (n_samples, seq_len))
         y = numpy.reshape(y, (n_samples, 1))
+        # X = numpy.apply_along_axis(lambda x: gaussian_filter(x, sigma=1.5), axis=-1, arr=X)
 
         if self.model is None:
             self.model = self.build_model(seq_len=seq_len)
@@ -45,7 +48,7 @@ class NNAnomalyDetector(object):
         y_train, y_val = y[:split_idx], y[split_idx:]
 
         checkpoint = ModelCheckpoint("best_anomaly_nn_weights.h5", monitor='val_acc', save_best_only=True, verbose=1)
-        early_stop = EarlyStopping(monitor='val_acc', patience=700, verbose=1)
+        early_stop = EarlyStopping(monitor='val_acc', patience=100, verbose=1)
         try:
             logger.debug("Beginning anomaly detector training..")
             self.model.fit(
@@ -63,11 +66,12 @@ class NNAnomalyDetector(object):
         n_samples = X.shape[0]
         seq_len = X.shape[1]
         X = numpy.reshape(X, (n_samples, seq_len))
+        # X = numpy.apply_along_axis(lambda x: gaussian_filter(x, sigma=1.5), axis=-1, arr=X)
         return self.model.predict([X]) > 0.5
 
     def save(self, prefix=None):
         if prefix is None:
-            prefix = "saved_models/RNNAnomalyDetector_%s.model" % int(time.time())
+            prefix = "saved_models/NNAnomalyDetector_%s.model" % int(time.time())
 
         logger.debug("Saving model to %s" % prefix)
 
